da5b51529969b938dd0c56f406a17dcefdb14654
Wanderson Sales <wandersonsales.dev@gmail.com>
2020-07-30 00:59:52 -0300

    Rootas configuradas (React-Router)

--- src/template/menu.jsx
+++ src/template/menu.jsx
@@ -12,9 +12,9 @@ export default props => (
       <div id="navbar" className="navbar-collapse collapse">
         <ul className="nav navbar-nav">
           <li><a href="#/todos">Tarefas</a></li>
-          <li><a href="#about">Sobre</a></li>
+          <li><a href="#/about">Sobre</a></li>
         </ul>
       </div>
     </div>
   </nav>
-)
+);
